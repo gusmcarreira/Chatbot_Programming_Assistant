0da91a9bfd3f9aea8d810046d041cd6c6961201c
gusmcarreira <90475975+gusmcarreira@users.noreply.github.com>
2022-04-12 04:02:47 +0100

    Update deploy_venv.sh

--- deploy_venv.sh
+++ deploy_venv.sh
@@ -5,11 +5,11 @@ git clone https://github.com/gusmcarreira/Chatbot_Programming_Assistant.git
 # Alterar pasta para a do Chatbot
 cd Chatbot_Programming_Assistant/Chatbot
 # Criar ambiente virtual
-python -m venv venv
+python3 -m venv venv
 # Instalar o RASA
-venv/bin/python -m pip install rasa==3.1
+venv/bin/python3 -m pip install rasa==3.1
 # Instalar outras bibliotecas necessárias
-venv/bin/pip install Transformers 
-venv/bin/pip install Whoosh
+venv/bin/pip3 install Transformers 
+venv/bin/pip3 install Whoosh
 # A iniciar o RASA (de maneira a perimitir a integração no website) e o servidor das ações (das funções do Python)
 venv/bin/rasa run --enable-api --cors="*" --endpoints "endpoints.yml" & venv/bin/rasa run actions
