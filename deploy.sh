7bc04a7fae002e56dfaae450277f6bca55f81d03
gusmcarreira <90475975+gusmcarreira@users.noreply.github.com>
2022-04-12 03:53:56 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 # Apagar caso não seja a primeira vez
-rm -r Chatbot_Programming_Assistant
+sudo rm -r Chatbot_Programming_Assistant
 # Clonar o git
 git clone https://github.com/gusmcarreira/Chatbot_Programming_Assistant.git
 # Alterar pasta para a do Chatbot
 cd Chatbot_Programming_Assistant/Chatbot
 # Instalar o RASA
-python -m pip install rasa
+python -m pip install rasa==3.1
 # Instalar outras bibliotecas necessárias
-pip install Transformers && pip install Whoosh
+pip install Transformers 
+pip install Whoosh
 # A iniciar o RASA (de maneira a perimitir a integração no website) e o servidor das ações (das funções do Python)
-rasa run --enable-api --cors="*" --endpoints "endpoints.yml" && rasa run actions
+rasa run --enable-api --cors="*" --endpoints "endpoints.yml" & rasa run actions
