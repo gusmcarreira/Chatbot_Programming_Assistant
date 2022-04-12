a5681764c3d65e5f9c0acb33d13f532e1255c5ea
gusmcarreira <90475975+gusmcarreira@users.noreply.github.com>
2022-04-12 03:52:30 +0100

    Update deploy_venv.sh

--- deploy_venv.sh
+++ deploy_venv.sh
@@ -9,6 +9,7 @@ python -m venv venv
 # Instalar o RASA
 venv/bin/python -m pip install rasa==3.1
 # Instalar outras bibliotecas necessárias
-venv/bin/pip install Transformers && venv/bin/pip install Whoosh
+venv/bin/pip install Transformers 
+venv/bin/pip install Whoosh
 # A iniciar o RASA (de maneira a perimitir a integração no website) e o servidor das ações (das funções do Python)
 venv/bin/rasa run --enable-api --cors="*" --endpoints "endpoints.yml" & venv/bin/rasa run actions
