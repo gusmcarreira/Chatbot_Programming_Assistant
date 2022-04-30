24e0942edc98ff86e633073744704e2d3f5ac24b
gusmcarreira <90475975+gusmcarreira@users.noreply.github.com>
2022-04-30 23:21:11 +0100

    Question Answering Model Local

--- Chatbot/custom/modelqa.py
+++ Chatbot/custom/modelqa.py
@@ -3,15 +3,15 @@ from transformers import pipeline
 from paths import question_answering_model
 import re
 #from get_whole_answer import get_wanted_whole_answer
-#from paths import doc_idx_directory
+# from paths import doc_idx_directory
 #from search_documents import search_in_documents
-
+# import spacy
+# from nltk.corpus import stopwords
 
 def answer_questions_model(context, question):
     context = re.sub('Sugestões\:.+[\-\#\-]*', '', context)
     nlp = pipeline("question-answering", model=question_answering_model)
     result = nlp(question=question, context=context)
-
     return [result['answer'], round(result['score'], 4)]
 
 
