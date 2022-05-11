92b7b1f51193ba0565533594dd67dea9649435e8
gusmcarreira <90475975+gusmcarreira@users.noreply.github.com>
2022-05-12 00:15:39 +0100

    Code Information simplified

--- Chatbot/custom/CodeInformation/code_information.py
+++ Chatbot/custom/CodeInformation/code_information.py
@@ -1,5 +1,4 @@
 import ast
-import astor
 
 class CodeInformation(ast.NodeVisitor):
     def __init__(self):
@@ -10,53 +9,49 @@ class CodeInformation(ast.NodeVisitor):
         # Check if it comes from print
         self.isPrint = False
         self.ifControl = {}
+
     # ------------------------ VARIABLES --------------------
     def visit_Assign(self, node):
-        self.all_concepts_map.append("Declaração de variável")
-        # Var name
+        var_meaning = "Declaração de variável COM "
         var_name = self.visit(node.targets[0])[1]
-        # Var value
         var_value = self.visit(node.value)
 
-        if var_value:
-            # Store var information
-            self.var_map[var_name] = var_value
-            if var_value[0] == "Constant":
-                self.all_concepts_map.append(self.value_meaning(var_value[1]))
-            elif var_value[0] == "Op":
-                self.all_concepts_map.append(var_value[1])
-        return
+        if var_value[0] == "Call":
+            self.all_concepts_map.append(var_meaning + var_value[1])
+            var_value = var_value[2]
+        elif var_value[0] == "Constant":
+            self.all_concepts_map.append(var_meaning + self.value_meaning(var_value[2]))
+            var_value = var_value[2]
+        elif var_value[0] == "Op":
+            self.all_concepts_map.append(var_meaning + var_value[1])
+            var_value = var_value[2]
+
+        self.var_map[var_name] = var_value
     # =============================== FUNCTIONS ===============================
     def visit_FunctionDef(self, node):
-        self.all_concepts_map.append("Declaração de função")
+        func_name = node.name
+        func_args = node.args.args
 
-        self.all_concepts_map.append("[INICIO ESCOPO FUNÇÃO]")
-
+        if len(func_args) == 0:
+            self.all_concepts_map.append("Declaração de função")
+        else:
+            self.all_concepts_map.append("Declaração de função com argumento/s")
         for concept in node.body:
             self.visit(concept)
-
-        self.all_concepts_map.append("[FINAL ESCOPO FUNÇÃO]")
-
         return
 
+    def visit_Return(self, node):
+        self.all_concepts_map.append("Retorno de função")
     # ============================== CONDITIONS ===============================
     def visit_If(self, node):
         if node.col_offset not in self.ifControl:
-            wanted_ident_start = "[INICIO ESCOPO IF]"
-            wanted_ident_end = "[FINAL ESCOPO IF]"
             self.all_concepts_map.append("Declaração de condicional (if)")
         else:
             if self.ifControl[node.col_offset] == "elif":
-                wanted_ident_start = "[INICIO ESCOPO ELIF]"
-                wanted_ident_end = "[FINAL ESCOPO ELIF]"
                 self.all_concepts_map.append("Declaração de condicional (elif)")
-
-        self.all_concepts_map.append(wanted_ident_start)
 
         for concept in node.body:
             self.visit(concept)
-
-        self.all_concepts_map.append(wanted_ident_end)
 
         if node.orelse:
             for arg in node.orelse:
@@ -65,126 +60,122 @@ class CodeInformation(ast.NodeVisitor):
                     self.visit(arg)
                 else:
                     self.all_concepts_map.append("Declaração de condicional (else)")
-
-                    self.all_concepts_map.append("[INICIO ESCOPO ELSE]")
-
                     for concept in node.body:
                         self.visit(concept)
-
-                    self.all_concepts_map.append("[FINAL ESCOPO ELSE]")
 
         return
 
     # ============================= REPETITIONS ===============================
     def visit_For(self, node):
         self.all_concepts_map.append("Declaração de repetição FOR")
-
-        self.all_concepts_map.append("[INICIO ESCOPO FOR]")
-
         for concept in node.body:
             self.visit(concept)
-
-        self.all_concepts_map.append("[FINAL ESCOPO FOR]")
-
         return
 
     def visit_While(self, node):
         self.all_concepts_map.append("Declaração de repetição WHILE")
-
-        self.all_concepts_map.append("[INICIO ESCOPO WHILE]")
-
         for concept in node.body:
             self.visit(concept)
 
-        self.all_concepts_map.append("[FINAL ESCOPO WHILE]")
-
-        return
     # ------------------ FUNCTION CALLS -----------------
     def visit_Call(self, node):
         func_name_info = self.visit(node.func)
         func_name = func_name_info[1]
         func_args = node.args
-
-        if self.func_natural_meaning(func_name):
-            self.all_concepts_map.append(self.func_natural_meaning(func_name))
-            # ---------------- INPUT --------------
-            if func_name == "input":
-                return ["Call", "str"]
-            # ---------------- PRINT --------------
-            elif func_name == "print":
-                if len(func_args) > 1:
-                    self.all_concepts_map.append("Concatenação")
+        if func_name == "input":
+            return ["Call", self.func_natural_meaning(func_name), "str"]
+        elif func_name == "print":
+            print_meaning = "Impressão DE "
+            if len(func_args) > 1:
+                print_meaning = print_meaning + "concatenação DE "
+                for index, arg in enumerate(func_args):
+                    arg = self.visit(arg)
+                    if arg[0] == "Name":
+                        print_meaning = print_meaning + "valor de variável"
+                    elif arg[0] == "Constant":
+                        print_meaning = print_meaning + self.value_meaning(arg[2])
+                    if index + 1 != len(func_args):
+                        print_meaning = print_meaning + " COM "
+            else:
+                arg = self.visit(func_args[0])
+                if arg[0] == "Name":
+                    print_meaning = print_meaning + "valor de variável"
                 else:
-                    func_arg_print = self.visit(func_args[0])
-
-                    if func_arg_print[0] == "Name":
-                        self.all_concepts_map.append("variável (já declarada)")
-                    elif func_arg_print[0] == "Constant":
-                        self.all_concepts_map.append(self.value_meaning(func_arg_print[1]))
-                    elif func_arg_print[0] == "Op":
-                        self.all_concepts_map.append(func_arg_print[1])
-            # ---------------- CONVERSIONS --------------
-            elif func_name == "int" or func_name == "str" or func_name == "float":
-                self.all_concepts_map.append("Leitura de dado do teclado")
-                return ["Call", func_name]
+                    print_meaning = print_meaning + self.value_meaning(arg[2])
+            self.all_concepts_map.append(print_meaning)
+        elif self.func_natural_meaning(func_name):
+            return ["Call", self.func_natural_meaning(func_name), func_name]
+        else:
+            if func_args:
+                self.all_concepts_map.append("Chamada de função com argumento/s")
+            else:
+                self.all_concepts_map.append("Chamada de função")
         return
 
     # ------------------ OPERATIONS -----------------
     def visit_BinOp(self, node):
-        var_left = self.visit(node.left)
-
+        # --> Operação mais complexa <--
         if isinstance(node.left, ast.BinOp) or isinstance(node.right, ast.BinOp):
-            return ["Op", astor.to_source(node)[1:-2]]
-            #return ["Op", "equação matemática"]
-
+            return ["Op", "equação matemática", "float"]
+        # ------------------------------
+        var_left = self.visit(node.left)
         var_right = self.visit(node.right)
         op = ast.dump(node.op)
-
-        if var_left[0] == "Name":
-            var_left = self.var_map[var_left[1]][1]
-        elif var_left[0] == "Constant":
-            if len(var_right) == 3:
-                var_left = [var_left[1], var_left[2]]
+        op_meaning = ""
+        op_type = ""
+        # ---> OPERAÇÃO COM VARIÁVEIS <---
+        if var_left[0] == "Name" and var_right[0] == "Name":
+            var_left_value = self.var_map[var_left[1]]
+            var_right_value = self.var_map[var_right[1]]
+            # --> CONCATENAÇÃO <--
+            if var_left_value == "str" and var_right_value == "str":
+                op_meaning = "concatenação de variáveis"
+                op_type = "string"
+            # --> OPERAÇÃO <--
             else:
-                var_left = var_left[1]
-        if var_right[0] == "Name":
-            var_right = self.var_map[var_right[1]][1]
-        elif var_right[0] == "Constant":
-            if len(var_right) == 3:
-                var_right = [var_right[1], var_right[2]]
+                op_meaning = self.operation_meaning(op)
+                if var_left_value == "float" or var_right_value == "float":
+                    op_type = "float"
+                else:
+                    op_type = "int"
+        # ---> OPERAÇÃO COM VARIÁVEL E CONSTANTE <---
+        elif var_left[0] == "Name" and var_right[0] == "Constant":
+            if var_right[2] == "str":
+                op_meaning = "concatenação de variável COM texto"
+                op_type = "string"
             else:
-                var_right = var_right[1]
-
-        if (var_left == "str" or var_left[1] == "str") and (var_right == "str" or var_right[1] == "str"):
-            return ["Op", "Concatenação"]
-
+                op_meaning = self.operation_meaning(op)
+        # ---> OPERAÇÃO COM CONSTANT E VARIÁVEL <---
+        elif var_left[0] == "Constant" and var_right[0] == "Name":
+            if var_right[2] == "str":
+                op_meaning = "concatenação de texto COM variável"
+                op_type = "string"
+            else:
+                op_meaning = self.operation_meaning(op)
+                if var_left[2] == "float" or var_left[2] == "float":
+                    op_type = "float"
+                else:
+                    op_type = "int"
+        # ---> OPERAÇÃO COM CONSTANTES <---
         else:
-            if isinstance(var_left, list) and isinstance(var_right, list):
-                return ["Op", self.operation_meaning(op) + " de numeros"]
-            elif isinstance(var_left, list):
-                return ["Op", self.operation_meaning(op) + " de var por " + str(var_left[1])]
-            elif isinstance(var_right, list):
-                return ["Op", self.operation_meaning(op) + " de var por " + str(var_right[1])]
-            else:
-                return ["Op", self.operation_meaning(op) + " de variáveis"]
-
-        return
+            op_meaning = self.operation_meaning(op)
+        return ["Op", op_meaning, op_type]
     # ------------------- NAMES ------------------
     def visit_Name(self, node):
         return ["Name", node.id]
 
     # ----------------- CONSTANTS ----------------
     def visit_Constant(self, node):
-        return ["Constant", type(node.value).__name__, node.value]
+        return ["Constant", node.value, type(node.value).__name__]
 
     def func_natural_meaning(self, func_name):
         try:
             func_map = {
-                        "input": "Leitura de dado do teclado",
+                        "input": "dado do teclado",
                         "print": "Impressão (print) de ...",
-                        "str": "Conversão de dado para texto (string)",
-                        "int": "Conversão de dado para inteiro (int)",
-                        "float": "Conversão de dado para número decimal (float)",
+                        "str": "conversão de dado para texto (string)",
+                        "int": "conversão de dado para inteiro (int)",
+                        "float": "conversão de dado para número decimal (float)",
                         }
             return func_map[func_name]
         except:
